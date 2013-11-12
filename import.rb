a407ec6a856d5da74d11de8b9c02bdb5553a6642
Blake Hilscher <blake@hilscher.ca>
2013-11-12 10:45:08 -0500

    generate a random number of datasets with a random number of rows

--- import.rb
+++ import.rb
@@ -1,4 +1,22 @@
 require 'quandl/data'
 require 'quandl/fabricate/data'
 
-puts Quandl::Fabricate::Data.rand(rows: 10, columns: 4).to_csv
+def names
+  ['Blake','Bob','Billy','Bean','Blair','Benjamin']
+end
+
+def generate_dataset
+  o = []
+  o << "#{names.sample}'s Sandbox Dataset #{Time.now}"
+  o << "Date, high, low, average"
+  o << Quandl::Fabricate::Data.rand(rows: rand(20)+10, columns: 4).to_csv
+  o << '----'
+  o.join("\n")
+end
+
+datasets = rand(5) + 2
+
+datasets.times do |i|
+  puts generate_dataset
+  sleep(1)
+end
